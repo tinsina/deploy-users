0257fa96f921ca11e837a33438dc6ef9b98bf186
tinsina <tinsina@live.fr>
2020-09-24 12:18:21 +0000

    update password message on linux script

--- linux_users.sh
+++ linux_users.sh
@@ -2,7 +2,7 @@
 # Script to add a user to Linux system
 if [ $(id -u) -eq 0 ]; then
 	read -p "Enter username : " username
-	read -s -p "Enter password : " password
+	read -s -p "Enter strong password : " password
 	egrep "^$username" /etc/passwd >/dev/null
 	if [ $? -eq 0 ]; then
 		echo "$username exists!"
